47a7758adcb88409a7bae6582c51d6dc2a2ad3b9
nikoe14 <nikoe14@gmail.com>
2015-08-29 12:12:56 -0300

    main updated

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,9 @@
  * Created by Nico on 8/27/15.
  */
 public class Main {
-    public void main (String[] args) {
-    Maso Autos = new Maso();
+    public static void main (String[] args) {
+    Maso autos = new Maso();
+       autos.setName("Nicolas");
+        System.out.println(autos.getName());
     }
 }
